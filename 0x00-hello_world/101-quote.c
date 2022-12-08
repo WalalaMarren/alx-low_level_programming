0a4d4e274a4fad98b372c5a88897453fc3445b4e
WalalaMarren <marrenanyangonyagaya@gmail,com>
2022-12-08 16:02:18 +0000

    Hello, World

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always success (0)
+ */
+int main(void)
+{
+	char ch = "and that piece of art is useful" - Dora Korpar, 2015-10-19";
+
+	putchar(ch);
+	return (0);
+}
