67ec78f02828b7fa089ea7d51d9067bae87ebfe5
WalalaMarren <marrenanyangonyagaya@gmail,com>
2022-12-09 21:18:42 +0000

    Variables if else and while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char d;
+
+	for (d = '48'; d <= '57'; d++)
+		putchar(d);
+	putchar('\n');
+
+	return (0);
+}
