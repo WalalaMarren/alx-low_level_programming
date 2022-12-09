140fe55e4ef56c601d87fcd71a91245bd79553a2
WalalaMarren <marrenanyangonyagaya@gmail,com>
2022-12-09 04:11:50 +0000

    Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -6,8 +7,7 @@
  */
 int main(void)
 {
-	char ch = "and that piece of art is useful" - Dora Korpar, 2015-10-19";
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 
-	putchar(ch);
-	return (0);
+	return(1);
 }
