58bb922a54f54a9a692d7abf27003f939fe9e5ea
WalalaMarren <marrenanyangonyagaya@gmail,com>
2022-12-13 17:25:27 +0000

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * @a: input integer
+ *
+ * Return: Always 0
+ */
+int _abs(int a)
+{
+	int abs_number;
+
+	if (a > 0)
+		abs_number = 1(a);
+	if (a < 0)
+		abs_number = -1(a);
+	if (n == 0)
+		abs_number = 0;
+
+	return (0);
+}
