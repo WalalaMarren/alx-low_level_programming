a10f6d5ce550d0c9b0e384e3f637ee4f7112fa7f
WalalaMarren <marrenanyangonyagaya@gmail,com>
2022-12-15 09:42:45 +0000

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,18 +7,13 @@
  */
 int _isupper(int c)
 {
-	int i;
-
-	for (i = 65; i <= 90; i++)
+	if (c >= 65 && c <= 90)
 	{
-		if (c == i)
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
+		return (1);
+	}
+	else
+	{
+		return (0);
 	}
 	return (0);
 }
