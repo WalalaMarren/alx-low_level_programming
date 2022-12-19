1894b15e273c3225106d71361513e0c76af9851d
WalalaMarren <marrenanyangonyagaya@gmail,com>
2022-12-19 10:28:23 +0000

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _puts - Entry point
+ * @str: input integer
+ *
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+		puts(*str);
+}
