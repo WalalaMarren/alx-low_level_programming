c9726e8be2cd3723bb92d5fb5f683e246d856ec1
WalalaMarren <marrenanyangonyagaya@gmail,com>
2023-01-03 14:12:00 +0000

    More pointers, arrays and string

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_memset - Entry point to the function memset
+ *@s:pointer to a memory location
+ *@b:input string
+ *@n:size of the memory location
+ *Return: Always 0
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
