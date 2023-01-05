50367007618ed4b50e8f5cec506b9cfb035dccc0
WalalaMarren <marrenanyangonyagaya@gmail,com>
2023-01-05 15:35:32 +0000

    Recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  *factorial - entry point
+  *@n: input integer
+  *Return: Always 0
+  */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0 || n == 1)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
