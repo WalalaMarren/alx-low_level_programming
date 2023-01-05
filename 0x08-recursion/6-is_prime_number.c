4f586d63e56f916eecf09d02d1a8273ac2513b88
WalalaMarren <marrenanyangonyagaya@gmail,com>
2023-01-05 17:38:11 +0000

    Recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  *is_prime_number - entry point
+  *@n: input integer
+  *Return: always 0
+  */
+int is_prime_number(int n)
+{
+	if (n % 1 == 0 && n % n == 0)
+		return (1);
+	else
+		return (0);
+}
