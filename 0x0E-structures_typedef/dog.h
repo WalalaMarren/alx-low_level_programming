7fc274c224c3d645b069754965c44a8dbb8a2754
WalalaMarren <marrenanyangonyagaya@gmail,com>
2023-01-17 15:48:01 +0000

    structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef _DOG_H_
 #define _DOG_H_
-
+/**
+  *struct dog - structure for a dog
+  *@name: name of the dog
+  *@age: age of the dog
+  *@owner: owner of the dog
+  */
 struct dog
 {
 	char *name;
