12b67bc430d604ac679a5859b6d2dc136a8678a5
WalalaMarren <marrenanyangonyagaya@gmail,com>
2023-01-19 05:00:08 +0000

    Function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - entry point
+ * @name: name to be printed
+ * @f: pointer to function that prints name
+ * Return: always 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * array_iterator - entry point
+ * @action: pointer to a function
+ * @array: array of function
+ * @size: the size for the array
+ * Return: always 0
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
