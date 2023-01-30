2c703d4afa76cf1750408769db5a6f64d246db68
WalalaMarren <marrenanyangonyagaya@gmail,com>
2023-01-30 15:31:18 +0000

    linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - entry point
+ * @h: pointer to a struct node
+ *
+ * Return: Always 0
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		if (h->str)
+			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
